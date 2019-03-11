161c825dde6c244834ca1833406aa3d3f90ffb3e
haythemsahlia <haithemsahlia@gmail.com>
2019-03-11 11:18:59 +0100

    hs change home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    eSolutions
+                    Najda Assistance
                 </div>
 
                 <div class="links">
