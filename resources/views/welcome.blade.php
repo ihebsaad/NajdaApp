28134e70eef6bf5e33f0b644735efcb290e09136
iheb <ihebsaad@gmail.com>
2019-03-11 11:09:49 +0100

    test changing home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    eSolutions
                 </div>
 
                 <div class="links">
