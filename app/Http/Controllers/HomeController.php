84ed1794729369d50c228395c9531c6ef77398ef
haythemsahlia <haithemsahlia@gmail.com>
2019-03-19 15:33:53 +0100

    interface agent

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+ 
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //for create controller - php artisan make:controller AutocompleteController
+
+    function index()
+    {
+        return view('najda');
+    }
+    function test ()
+    {}
+}
