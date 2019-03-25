677ad443b3c62e097dc8b815f8e426fd6b7c609a
iheb <ihebsaad@gmail.com>
2019-03-25 11:38:05 +0100

    ajouter alerte + maboite ..

---
+++ resources/views/emails/maboite.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin1
+ * Date: 22/03/2019
+ * Time: 15:14
+ */
---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin1
+ * Date: 22/03/2019
+ * Time: 09:29
+ */
