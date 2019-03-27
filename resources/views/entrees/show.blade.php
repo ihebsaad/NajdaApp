58221642256b68bec87258c1915c862fcdb2a621
haythemsahlia <haithemsahlia@gmail.com>
2019-03-27 15:58:02 +0100

    index cng

--- resources/views/entrees/show.blade.php
+++ resources/views/entrees/show.blade.php
@@ -100,10 +100,22 @@ use App\Http\Controllers\AttachementsController;
                                             @for ($i = 1; $i <= $entree->nb_attach; $i++)
                                                 <div class="tab-pane fade in" id="pj<?php echo $i; ?>">
 
-                                                    <?php
-                                                       $names = array_fetch($attachs, 'name');
-                                                       echo $names;
-                                                    ?>
+                                                    <h4><b style="font-size: 13px;">{{ $attachs[1]->nom }}</b></h4>
+
+                                                    
+                                                    
+                                                    @switch($attachs[1]->type)
+                                                        @case('docx')
+                                                            <iframe src="https://view.officeapps.live.com/op/view.aspx?src={{ URL::asset('storage'.$attachs[1]->path) }}" frameborder="0" style="width:100%;min-height:640px;"></iframe>
+                                                            @break
+
+                                                        @case('pdf')
+                                                            <iframe src="{{ URL::asset('storage'.$attachs[1]->path) }}" frameborder="0" style="width:100%;min-height:640px;"></iframe>
+                                                            @break
+
+                                                        @default
+                                                            <span>Type de fichier non reconnu ....  <a href="{{ URL::asset('storage'.$attachs[1]->path) }}" download>Télécharger</a> </span>
+                                                    @endswitch
                                                     
                                                 </div>
                                             @endfor
