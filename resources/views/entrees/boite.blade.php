cab0a2fac72d8295ea51022bdd983ebd17b0b213
iheb <ihebsaad@gmail.com>
2019-03-28 15:29:55 +0100

    update styles inbox

--- resources/views/entrees/boite.blade.php
+++ resources/views/entrees/boite.blade.php
@@ -20,9 +20,9 @@ $dossiers = Dossier::get();
         .email {background:#ececec; position:relative; margin-bottom:15px; padding:5px; border-style: solid!important; border-color: #cccccc; border-width: 1px!important;}
         .email-body{border:1px solid #bcbcbc; border-left:0; height:136px;}
         .email .subject{float:left; width:100%; font-size:15px;font-weight:600; color:#00a7e2;}
-        .email .subject small{display:block; font-size:12px; color:#232323;}
+        .email .subject small{display:block; font-size:14px; color:#232323;}
         .email .stats{float:left; width:100%; margin-top:10px;}
-        .email .stats span{float:left; margin-right:10px; font-size:12px;}
+        .email .stats span{float:left; margin-right:10px; font-size:14px;}
         .email .stats span i{margin-right:7px; color:#7ecce7;}
         .email .fav-box{position:absolute; right:10px; font-size:15px; top:4px; color:#E74C3C;}
     </style>
@@ -38,15 +38,15 @@ $dossiers = Dossier::get();
                 <div class="email">
                     <div class="fav-box">
                         <a href="{{action('EntreesController@show', $entree['id'])}}" class="btn btn-sm btn-primary btn-responsive">
-                            <i class="fa fa-fw fa-eye"></i>
+                            <i class="fa fa-lg fa-fw fa-eye"></i>
                             Ouvrir
                         </a>
                     </div>
                       <div class="media-body pl-3">
                         <div class="subject"><a  href="{{action('EntreesController@show', $entree['id'])}}" >{{$entree->sujet}}</a><small>{{$entree->emetteur}}</small></div>
                         <div class="stats">
-                            <span><i class="fa fa-fw fa-clock-o"></i><?php echo  date('d/m/Y H:i', strtotime($entree->reception)) ; ?></span>
-                            <span><i class="fa fa-fw fa-paperclip"></i><b>({{$entree->nb_attach}})</b> Attachements</span>
+                            <span><i class="fa fa-lg fa-fw fa-clock-o"></i><?php echo  date('d/m/Y H:i', strtotime($entree->reception)) ; ?></span>
+                            <span><i class="fa fa-lg fa-fw fa-paperclip"></i><b>({{$entree->nb_attach}})</b> Attachements</span>
                         </div>
                       </div>
                 </div>
