585382778cf326bb8b58f463ac49cd187e4a92eb
haythemsahlia <haithemsahlia@gmail.com>
2019-04-04 09:02:54 +0100

    remove var dossiers from index function in controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,9 +29,8 @@ class HomeController extends Controller
 
         //  $countries = DB::table('apps_countries')->pluck('id', 'country_name');;
         $countries = DB::table('apps_countries')->select('id', 'country_name')->get();
-        $dossiers = Dossier::get();
 
-        return view('home', ['countries' => $countries,'dossiers' => $dossiers]);
+        return view('home', ['countries' => $countries]);
      }
 
     function fetch(Request $request)
