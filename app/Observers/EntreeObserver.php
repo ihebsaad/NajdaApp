e0c15d6d53acbb1a039c47be011ff90e194b9338
iheb <ihebsaad@gmail.com>
2019-04-05 13:02:04 +0100

    test

--- app/Observers/EntreeObserver.php
+++ app/Observers/EntreeObserver.php
@@ -12,7 +12,7 @@ class EntreeObserver
      public function created(Entree $entree)
 	 {
 	     dd('bonjour');
-		Auth::user()->notify(new Notif_Suivi_Doss($entree)); 
+	//	Auth::user()->notify(new Notif_Suivi_Doss($entree));
 		 
 	 }
 
