1c6c17bb3cddf4f632df8668adee98f552cb0090
kbskhaled <kbskhaled@gmail.com>
2019-04-05 11:29:05 +0100

    ajout observer+notif+appprovider

---
+++ app/Observers/EntreeObserver.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Observers;
+
+use App\Entree;
+use App\User;
+use App\Notifications\Notif_Suivi_Doss;
+use Auth;
+
+class EntreeObserver
+{
+     public function created(Entree $entree)
+	 {
+		Auth::user()->notify(new Notif_Suivi_Doss($entree)); 
+		 
+	 }
+
+}
