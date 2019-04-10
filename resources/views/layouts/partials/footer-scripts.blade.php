a437997d1ad288c923ac0d4914030a26ca3ea5b1
kbskhaled <kbskhaled@gmail.com>
2019-04-10 11:21:08 +0100

    footer scripts js

--- resources/views/layouts/partials/footer-scripts.blade.php
+++ resources/views/layouts/partials/footer-scripts.blade.php
@@ -135,7 +135,7 @@
 
 <script>
 
-/*
+
  
    // var userId = $('meta[name="userId"]').attr('content')
    Echo.private('App.User.{{Auth::id()}}').notification((notification) => {
@@ -151,5 +151,5 @@
           // alert (this.notifications);
         });
 
-        */
+        
   </script>
