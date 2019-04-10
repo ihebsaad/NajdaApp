86c71a08b3be456b3573ecce632ff87f6b01b6cb
kbskhaled <kbskhaled@gmail.com>
2019-04-10 14:26:48 +0100

    update observer

--- app/Observers/EntreeObserver.php
+++ app/Observers/EntreeObserver.php
@@ -6,13 +6,16 @@ use App\Entree;
 use App\User;
 use App\Notifications\Notif_Suivi_Doss;
 use Auth;
+use Notification;
 
 class EntreeObserver
 {
      public function created(Entree $entree)
 	 {
-	  //   dd('kbs');
-		Auth::user()->notify(new Notif_Suivi_Doss($entree));
+	  // dd('kbs');
+		//Auth::user()->notify(new Notif_Suivi_Doss($entree));
+		$user= User::get();
+       Notification::send($user, new Notif_Suivi_Doss($entree));
 		 
 	 }
 
