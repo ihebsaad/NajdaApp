651b6d0437069f9001bae43e882685305ba9e5ca
haythemsahlia <haithemsahlia@gmail.com>
2019-04-24 17:25:47 +0100

    entree timestamp

--- app/Entree.php
+++ app/Entree.php
@@ -9,10 +9,11 @@ class Entree extends Model
     //
     protected $fillable = ['emetteur','sujet','contenu','nb_attach','reception','type','dossier','statut','mailid','affecte','notif','viewed'];
 
-   protected $dateFormat = 'Y-m-d H:i';
+   /*protected $dateFormat = 'Y-m-d H:i';
 
     public function getDateFormat()
     {
         return 'Y-m-d H:i:s.u';
     }
+    */
 }
