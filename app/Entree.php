021a6ce02e0cdad632374aaa03150137fc0283d6
iheb <ihebsaad@gmail.com>
2019-04-24 13:10:57 +0100

    update entree

--- app/Entree.php
+++ app/Entree.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Entree extends Model
 {
     //
-    protected $fillable = ['emetteur','sujet','contenu','nb_attach','reception','type','dossier','statut','updated_at','mailid','affecte','notif','viewed'];
+    protected $fillable = ['emetteur','sujet','contenu','nb_attach','reception','type','dossier','statut','mailid','affecte','notif','viewed'];
 
    protected $dateFormat = 'Y-m-d H:i';
 
