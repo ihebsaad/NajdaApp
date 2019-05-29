931d3d415a79eef86900490361f515f638962c88
iheb <ihebsaad@gmail.com>
2019-05-29 12:25:23 +0100

    fix prestataires error

--- app/Http/Controllers/PrestatairesController.php
+++ app/Http/Controllers/PrestatairesController.php
@@ -144,7 +144,9 @@ class PrestatairesController extends Controller
      */
     public function view($id)
     {
-        $dossiers = Dossier::all();
+        //$dossiers = Dossier::all();
+        $dossiers = app('App\Http\Controllers\DossiersController')->ListeDossiersAffecte();
+
         $typesprestations = TypePrestation::all();
       // $villes = DB::table('cities')->select('id', 'name')->get();
         $villes = Ville::all();
@@ -165,7 +167,7 @@ class PrestatairesController extends Controller
         $evaluations =   Evaluation::where('prestataire', $id)->get();
 
 
-        return view('prestataires.view',['emails'=>$emails,'evaluations'=>$evaluations,'gouvernorats'=>$gouvernorats,'relationsgv'=>$relationsgv,',dossiers' => $dossiers,'villes'=>$villes,'typesprestations'=>$typesprestations,'relations'=>$relations,'prestations'=>$prestations], compact('prestataire'));
+        return view('prestataires.view',['emails'=>$emails,'evaluations'=>$evaluations,'gouvernorats'=>$gouvernorats,'relationsgv'=>$relationsgv,'dossiers' => $dossiers,'villes'=>$villes,'typesprestations'=>$typesprestations,'relations'=>$relations,'prestations'=>$prestations], compact('prestataire'));
 
     }
 
