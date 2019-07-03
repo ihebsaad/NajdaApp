c6714c335c2a5147f6d48eb47f0655f9de4d2deb
kbskhaled <kbskhaled@gmail.com>
2019-07-03 15:25:47 +0100

    maj deleMiss+delegMissHis

--- app/DelegMiss.php
+++ app/DelegMiss.php
@@ -8,7 +8,7 @@ class DelegMiss extends Model
 {
     //
 
-     protected $table='Deleg_Miss';
+     protected $table='deleg_miss';
 
     protected $fillable = [
        'util_affecteur','util_affecte','id_mission','id_dossier','id_seance','date_affectation','statut', 'created_at','updated_at','deleted_at'
--- app/DelegMissHis.php
+++ app/DelegMissHis.php
@@ -8,7 +8,7 @@ class DelegMissHis extends Model
 {
     //
 
-     protected $table='Deleg_Miss_his';
+     protected $table='deleg_miss_his';
 
     protected $fillable = [
        'util_affecteur','util_affecte','id_mission','id_dossier','id_seance','date_affectation','statut', 'created_at','updated_at','deleted_at'
