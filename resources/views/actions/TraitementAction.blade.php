4b09b64bf180ccb51ce358459ac958e1dbf5c5b4
kbskhaled <kbskhaled@gmail.com>
2019-07-04 19:09:06 +0100

    maj traitement action

--- resources/views/actions/TraitementAction.blade.php
+++ resources/views/actions/TraitementAction.blade.php
@@ -58,7 +58,71 @@
    <!--<form action="{{ url('dossier/Mission/TraitercommentAction/'.$Action->Mission->dossier->id.'/'.$Action->Mission->id.'/'.$Action->id)}}">-->
 
     <br>
+     <div class="row">
      
+     <div class="col-sm-2">
+
+
+      <div class="btn-group">
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                    <i class="fa fa-envelope"></i> Email <i class="fa fa-angle-down"></i>
+                </button>
+                <ul class="dropdown-menu pull-right">
+                    <li>
+                        <a href="" class="sendMail" data-dest="client" style="font-size:17px;height:30px;margin-bottom:5px;">
+                            Au client </a>
+                    </li>
+                    <li>
+                        <a href="" class="sendMail" data-dest="client" style="font-size:17px;height:30px;margin-bottom:5px;">
+                            Au Prestataire </a>
+                    </li>
+                    <li>
+                        <a href="" class="sendMail" data-dest="client" style="font-size:17px;height:30px;margin-bottom:5px;">
+                            A l'assuré </a>
+                    </li>
+
+                </ul>
+     </div>&nbsp
+     </div>
+    <div class="col-sm-1">
+      <!--<button class="btn btn-default" >sms</button>&nbsp-->
+      <button type="button" class="btn btn-default"  >
+                    <a style="color:black" href=""> SMS</a>
+                </button>
+     </div>
+     <div class="col-sm-2">
+      <!--<button class="btn btn-default">téléphone </button>&nbsp;-->
+       <button type="button" class="btn btn-default" >
+              
+                    Téléphone
+
+                </button>
+     </div>
+     <div class="col-sm-1">
+      <!--<button class="btn btn-default">fax</button>&nbsp-->
+      <button type="button" class="btn btn-default" >
+          <a style="color:black" href=""> Fax</a>
+      </button>
+     </div>
+     <div class="col-sm-2">
+     <button type="button" class="btn btn-default"  >
+                    <a style="color:black" href=""> Prestation</a>
+                </button>
+     </div>
+     <div class="col-sm-2">
+      <button type="button" class="btn btn-default"  >
+                    <a style="color:black"  href="">Créer DOC</a>
+                </button>
+     </div>
+     <div class="col-sm-2">
+      <button type="button" class="btn btn-default"  >
+                    <a style="color:black"  href=""> Créer ORM</a>
+                </button>
+     </div>
+
+
+
+    </div>
 
     <!--<div class="row">
        
