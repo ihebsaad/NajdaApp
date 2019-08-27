68246b2fcda1123356a9496c7af2b2b73108d67c
kbskhaled <kbskhaled@gmail.com>
2019-08-27 14:18:50 +0100

    dosier+entree date

--- app/Dossier.php
+++ app/Dossier.php
@@ -112,12 +112,12 @@ class Dossier extends Model
 
     ];
 
-    protected $dateFormat = 'Y-m-d H:i:s';
+    /*protected $dateFormat = 'Y-m-d H:i:s';
 
     public function getDateFormat()
     {
         return 'Y-m-d H:i:s.u';
-    }
+    }*/
 /*
  public function activeMissions()
     {
