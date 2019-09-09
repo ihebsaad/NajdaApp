7c9fe14365224481693ad19abc6b6ffa5541e74a
kbskhaled <kbskhaled@gmail.com>
2019-09-09 09:55:51 +0100

    entree

--- app/Entree.php
+++ app/Entree.php
@@ -18,12 +18,12 @@ class Entree extends Model
         return $this->hasMany('App\Attachement');
     }
 
-  protected $dateFormat = 'Y-m-d H:i';
+  /*protected $dateFormat = 'Y-m-d H:i';
 
     public function getDateFormat()
     {
         return 'Y-m-d H:i:s.u';
-    }
+    }*/
 
 
 
