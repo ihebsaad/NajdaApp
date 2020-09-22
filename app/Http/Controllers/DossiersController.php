d150699a17c1f14f5fbcf5a1bba6b1b6c90a4427
haythemsahlia <haithemsahlia@gmail.com>
2020-09-22 13:36:26 +0100

    ajout ftags pour searchprest

--- app/Http/Controllers/DossiersController.php
+++ app/Http/Controllers/DossiersController.php
@@ -2539,10 +2539,11 @@ class DossiersController extends Controller
         $dossiers = $this->ListeDossiersAffecte();
 
         $evaluations=DB::table('evaluations')->get();
+        $tagdossier = $this->DossierTags($id,$ref);
 
 return view('dossiers.view',['datasearch'=>$datasearch,'phonesInt'=>$phonesInt,'phonesCl'=>$phonesCl,'phonesDossier'=>$phonesDossier,'evaluations'=>$evaluations,'intervenants'=>$intervenants,'prestataires'=>$prestataires,'gouvernorats'=>$gouvernorats,'specialites'=>$specialites,'client'=>$cl,
 'entite'=>$entite,'adresse'=>$adresse,   'emailads'=>$emailads,'dossiers'=>$dossiers,'entrees1'=>$entrees1,'envoyes1'=>$envoyes1,'communins'=>$communins,'typesprestations'=>$typesprestations,
-'attachements'=>$attachements,'entrees'=>$entrees,'prestations'=>$prestations,'typesMissions'=>$typesMissions,'Missions'=>$Missions,'envoyes'=>$envoyes,'documents'=>$documents,'ommi'=>$ommi], compact('dossier'));
+'attachements'=>$attachements,'entrees'=>$entrees,'prestations'=>$prestations,'typesMissions'=>$typesMissions,'Missions'=>$Missions,'envoyes'=>$envoyes,'documents'=>$documents,'ommi'=>$ommi,'ftags'=>$tagdossier], compact('dossier'));
 
 
     }
