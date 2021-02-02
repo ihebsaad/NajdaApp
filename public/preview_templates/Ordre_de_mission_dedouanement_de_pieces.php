6a60f8c884b54c3e59900b71419aa00a73d8c2d0
hammali sirine <hammalisirine120@gmail.com>
2021-02-02 23:47:18 +0100

    ajout prenom+civilite dedounement pieces+document generique

--- public/preview_templates/Ordre_de_mission_dedouanement_de_pieces.php
+++ public/preview_templates/Ordre_de_mission_dedouanement_de_pieces.php
@@ -63,23 +63,23 @@ mysqli_query($conn,"set names 'utf8'");
 
 // recuperation des prestataires HOTEL ayant prestations dans dossier
 
-$sqlvh = "SELECT id,name,phone_home,ville,ville_id FROM prestataires WHERE id IN (SELECT prestataire_id FROM prestations WHERE dossier_id=".$iddossier." AND effectue=1) AND id IN (SELECT prestataire_id FROM prestataires_type_prestations WHERE type_prestation_id = 40)";
+$sqlvh = "SELECT id,name,prenom,civilite,phone_home,ville,ville_id FROM prestataires WHERE id IN (SELECT prestataire_id FROM prestations WHERE dossier_id=".$iddossier." AND effectue=1) AND id IN (SELECT prestataire_id FROM prestataires_type_prestations WHERE type_prestation_id = 40)";
 
     $resultvh = $conn->query($sqlvh);
     if ($resultvh->num_rows > 0) {
 
         $array_prest = array();
         while($rowvh = $resultvh->fetch_assoc()) {
-            $array_prest[] = array('id' => $rowvh["id"],'name' => $rowvh["name"]  );
+            $array_prest[] = array('id' => $rowvh["id"],'name' => $rowvh["name"] ,"prenom" => $rowvh["prenom"],"civilite" => $rowvh["civilite"] );
         } }
-$sqlvha = "SELECT id,name,phone_home,ville,ville_id,id FROM prestataires WHERE id IN (SELECT prestataire_id FROM prestations WHERE dossier_id=".$iddossier." AND effectue=1)  AND id IN (SELECT prestataire_id FROM prestataires_type_prestations WHERE type_prestation_id = 22)";
+$sqlvha = "SELECT id,name,prenom,civilite,phone_home,ville,ville_id,id FROM prestataires WHERE id IN (SELECT prestataire_id FROM prestations WHERE dossier_id=".$iddossier." AND effectue=1)  AND id IN (SELECT prestataire_id FROM prestataires_type_prestations WHERE type_prestation_id = 22)";
 
     $resultvha = $conn->query($sqlvha);
     if ($resultvha->num_rows > 0) {
 
         $array_presta = array();
         while($rowvha = $resultvha->fetch_assoc()) {
-            $array_presta[] = array('id' => $rowvha["id"],"name" => $rowvha["name"]  );
+            $array_presta[] = array('id' => $rowvha["id"],"name" => $rowvha["name"]  ,"prenom" => $rowvha["prenom"],"civilite" => $rowvha["civilite"]);
         } }
 $sqlclient = "SELECT id,groupe,name FROM clients";
 	$resultclient = $conn->query($sqlclient);
@@ -312,10 +312,10 @@ foreach ($array_prest as $prest) {
 
 if(($prest['id'] === $id__prestataire)) {
     
-    echo '<option value="'.$prest["name"].'" id="'.$prest["id"].'" selected >'.$prest["name"].'</option>';}
+    echo '<option value="'.$prest["civilite"].' '.$prest["prenom"].' '.$prest["name"].'" id="'.$prest["id"].'" selected >'.$prest["civilite"].' '.$prest["prenom"].' '.$prest["name"].'</option>';}
 else {
     
-    echo '<option value="'.$prest["name"].'" id="'.$prest["id"].'">'.$prest["name"].'</option>';}
+  echo '<option value="'.$prest["civilite"].' '.$prest["prenom"].' '.$prest["name"].'" id="'.$prest["id"].'" >'.$prest["civilite"].' '.$prest["prenom"].' '.$prest["name"].'</option>';}
 }}
 ?>
 </select>
@@ -380,10 +380,10 @@ foreach ($array_presta as $presta) {
 
 if(($presta['id'] === $id__prestataire1)) {
     
-  echo '<option value="'.$presta["name"].'" id="'.$presta["id"].'" selected >'.$presta["name"].'</option>';}
+  echo '<option value="'.$presta["civilite"].' '.$presta["prenom"].' '.$presta["name"].'" id="'.$presta["id"].'" selected >'.$presta["civilite"].' '.$presta["prenom"].' '.$presta["name"].'</option>';}
 else {
     
-     echo '<option value="'.$presta["name"].'" id="'.$presta["id"].'">'.$presta["name"].'</option>';}
+    echo '<option value="'.$presta["civilite"].' '.$presta["prenom"].' '.$presta["name"].'" id="'.$presta["id"].'" >'.$presta["civilite"].' '.$presta["prenom"].' '.$presta["name"].'</option>';}
 }
     }
    
